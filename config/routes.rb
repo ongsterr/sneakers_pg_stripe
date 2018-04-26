bb345d90a306fd6d0b4a35c2918441e3eb206659
ongsterr <ong.chris11@gmail.com>
2018-04-26 16:24:48 +1000

    Update root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  root 'sneakers#index'
   devise_for :users
   resources :sneakers do
     member do
       post 'charge'
     end
   end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # post '/charge/:id/charge', to: 'sneakers#charge'
